56124aab84824e7c0672677902014af1211b72e0
Pawel Krupski <pankrupski@gmail.com>
2024-03-28 19:51:06 +0100

    first canister

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { Server, StableBTreeMap, ic } from 'azle';
 import express from 'express';
 
-/**
- * `messagesStorage` - it's a key-value datastructure that is used to store messages.
- * {@link StableBTreeMap} is a self-balancing tree that acts as a durable data storage that keeps data across canister upgrades.
- * For the sake of this contract we've chosen {@link StableBTreeMap} as a storage for the next reasons:
- * - `insert`, `get` and `remove` operations have a constant time complexity - O(1)
- * - data stored in the map survives canister upgrades unlike using HashMap where data is stored in the heap and it's lost after the canister is upgraded
- *
- * Brakedown of the `StableBTreeMap(string, Message)` datastructure:
- * - the key of map is a `messageId`
- * - the value in this map is a message itself `Message` that is related to a given key (`messageId`)
- *
- * Constructor values:
- * 1) 0 - memory id where to initialize a map.
- */
-
-/**
- This type represents a message that can be listed on a board.
- */
 
 class Message {
    id: string;
